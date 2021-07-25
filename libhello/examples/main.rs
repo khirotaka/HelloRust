21ab868128d49b88f6a192c6f02af771a9b6ceae
Hirotaka Kawashima <k.hirotaka@me.com>
2021-07-25 11:43:27 +0900

    乱数を使うように変更

--- libhello/examples/main.rs
+++ libhello/examples/main.rs
@@ -1,3 +1,5 @@
+use rand::{Rng, SeedableRng};
+
 use libhello::utils::math;
 use libhello::activations::structs;
 use libhello::activations::structs::Activation;
@@ -8,7 +10,14 @@ fn main() {
     let b = 10;
     println!("ans: {}", math::add(a, b));
 
-    let activation = structs::Sigmoid::new();
+    let sigmoid = structs::Sigmoid::new();
     let inputs: f32 = 0.05;
-    println!("Sigmoid: out {}", activation.forward(inputs));
+    println!("Sigmoid: out {}", sigmoid.forward(inputs));
+
+    let seed: [u8; 32] = [0; 32];   // SeedableRng::from_seed(seed) で 32個の要素が要求されているから
+    let mut rng: rand::rngs::StdRng = SeedableRng::from_seed(seed);
+
+    let relu = structs::ReLU::new();
+    let inputs: f32 = rng.gen();
+    println!("ReLU: out {}", relu.forward(inputs));
 }
